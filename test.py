92370fe343998486b67eaf751d86636afb00f2ab
Hardit Singh <hardit@outlook.com>
2018-05-01 23:10:58 +0200

    Simple Statistics caluclated for the unhedged MoM MSCI data

---
+++ test.py
@@ -0,0 +1,28 @@
+import csv
+import numpy as np
+from scipy.stats import kurtosis, skew
+
+
+def read_data():
+    input_file = open("/home/hardit/Desktop/yoyData.csv", "rb")
+    reader = csv.reader(input_file)
+
+    data = []
+
+    for row in reader:
+        data.append(float(row[0]))
+
+    print data
+    print "\nMean: %f" % np.mean(data)
+    print "Standard Deviation: %f" % np.std(data)
+    print "Kurtosis: %f" % kurtosis(data)
+    print "Skew: %f" % skew(data)
+
+    input_file.close()
+
+
+def main():
+    read_data()
+
+
+main()
